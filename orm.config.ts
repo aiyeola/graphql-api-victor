24bcf9a1a1e2d687e419be0461724ce6cbd36b0c
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-04-09 13:17:12 +0100

    - prepare codebase for production env

--- orm.config.ts
+++ orm.config.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 
+import { __prod__ } from './src/constants';
+
 dotenv.config();
 
 export default {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  logging: true,
+  logging: !__prod__,
   synchronize: true,
 } as const;
